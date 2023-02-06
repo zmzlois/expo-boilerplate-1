961ec91abc73780ac692830b7256490124d1bde2
pasqualintosh <pasqsac@yahoo.it>
2023-02-06 10:39:15 +0100

    remove src folder

--- src/Components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Brand } from './Brand'
-export { default as CharacterItem } from "./ListItems/CharacterItem"
-export { default as Loading } from "./Ui/Loading"
-export { default as Separator } from "./Ui/Separator"
